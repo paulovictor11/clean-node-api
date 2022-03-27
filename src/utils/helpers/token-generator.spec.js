03ca70d3e86deece8c09f2a13de47dd321d287d4
Paulo Victor <37181462+paulovictor11@users.noreply.github.com>
2022-03-27 11:46:49 -0300

    ensure TokenGenerator throws if no id is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -10,6 +10,10 @@ class TokenGenerator {
     if (!this.secret) {
       throw new MissingParamError('secret')
     }
+    if (!id) {
+      throw new MissingParamError('id')
+    }
+
     return jwt.sign(id, this.secret)
   }
 }
@@ -48,5 +52,12 @@ describe('', () => {
     const promise = sut.generate('any_id')
 
     expect(promise).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('should throw if no id is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
   })
 })
