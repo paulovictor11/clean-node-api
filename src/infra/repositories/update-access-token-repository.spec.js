b2d19e781cd4b556636da3a71bd8f439bb921f08
Paulo Victor <37181462+paulovictor11@users.noreply.github.com>
2022-03-28 15:48:07 -0300

    ensure UpdateAccessTokenRepository throws if no user model is provided

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -44,4 +44,19 @@ describe('Update Access Token Repository', () => {
 
     expect(updatedFakeUser.accessToken).toBe('valid_token')
   })
+
+  test('should throw if no userModel is provided', async () => {
+    const sut = new UpdateAccessTokenRepository()
+    const userModel = db.collection('users')
+    const fakeUser = await userModel.insertOne({
+      email: 'valid_email@mail.com',
+      name: 'any_name',
+      age: 50,
+      state: 'any_state',
+      password: 'hashed_password'
+    })
+    const promise = sut.update(fakeUser.insertedId, 'valid_token')
+
+    expect(promise).rejects.toThrow()
+  })
 })
